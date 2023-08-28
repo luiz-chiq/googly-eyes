ea4d567190c937a0ec3fb4ae5865f694dc339aaa
luiz-chiq <chiquetanoluiz@gmail.com>
2023-08-28 02:19:27 -0300

    feat: the eye follows the eye by the angle

--- src/components/Eye/Eye.js
+++ src/components/Eye/Eye.js
@@ -1,9 +1,21 @@
 import styles from "./Eye.module.css"
+import { useEffect, useState } from "react";
 
-function Eye({x, y}){
+function Eye({x, y, eyeX, eyeY}){
+    const [angle, setAngle] = useState(0)
+    let hypot = Math.hypot(eyeY - y, eyeX - x)/3
+    if (hypot > 20) hypot = 20
+
+    useEffect(() => {
+        let an = Math.atan2((eyeX - x), (eyeY - y)) * ((180/ Math.PI))
+        setAngle(an*-1 - 90)
+    })
+
     return(
-        <div className={styles.sclera}>
-                <div className={styles.pupil} style={{left: `${x}%`, top: `${y}%`}}/>
+        <div className={styles.sclera} style={{left: `${eyeX}px`, top: `${eyeY}px`}}>
+            <div className={styles.inner_sclera} style={{rotate: `${angle}deg`}}>
+                <div className={styles.pupil} style={{left: `${20 + hypot}%`}}/>
+            </div>
         </div>
     )
 }
--- src/components/Eyes/Eyes.js
+++ src/components/Eyes/Eyes.js
@@ -1,6 +1,6 @@
 import Eye from '../Eye/Eye'
 import {useState, useEffect} from 'react'
-import styles from './Eyes.module.css'
+// import styles from './Eyes.module.css'
 
 function Eyes({}){
     const [ angle, setAngle ] = useState(0)
@@ -12,39 +12,43 @@ function Eyes({}){
 
     useEffect(() => {
         document.onmousemove = (e) => {
-            let x1 = e.pageX;
-            let y1 = e.pageY;
-            
-            var ww = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-            var wh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-            
-            
-            let posx = (x1-(ww/2))
-            let posy = (y1-(wh/2))
-            let hipo = Math.hypot(posx, posy)
-    
-            distancex = x1/ww*50
-            distancey = y1/wh*50
-            
-            let distanceMx = Math.abs(distancex - 25)
-            let distanceMy = Math.abs(distancey - 25)
-            
-            setX(distancex);
-            setY(distancey);
-
-            document.onmousedown = (e) => {
-                console.log("disx:", distancex)
-                console.log("disMx:",distanceMx)
-                console.log("disy:", distancey)
-                console.log(((distanceMx + distanceMy) < 35)?"menor" : "maior")
-            }
+            setX(e.pageX);
+            setY(e.pageY);
         }
     })
 
     return(
-        <div className={styles.eyesContainer}>
-            <Eye x={x} y={y}/>
-            <Eye x={x} y={y}/>
+        <div>
+            <Eye x={x} y={y} eyeX={700} eyeY={400}/>
+            <Eye x={x} y={y} eyeX={820} eyeY={400}/>
+            {/* <Eye x={x} y={y} eyeX={300} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={400} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={500} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={600} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={700} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={800} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={900} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={1000} eyeY={100}/>
+            <Eye x={x} y={y} eyeX={100} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={200} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={300} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={400} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={500} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={600} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={700} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={800} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={900} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={1000} eyeY={200}/>
+            <Eye x={x} y={y} eyeX={100} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={200} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={300} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={400} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={500} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={600} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={700} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={800} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={900} eyeY={300}/>
+            <Eye x={x} y={y} eyeX={1000} eyeY={300}/> */}
         </div>
     )
 }
